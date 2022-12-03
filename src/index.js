9ac560c7c55d5c24f81458f085828756b342ad38
LucaPalminteri <lucapalminteri02@gmail.com>
2022-12-02 21:34:57 -0300

    add 404 file

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.put('/items/:id', updateItem);
 app.delete('/items/:id', deleteItem);
 
 app.use(function (req,res,next){
-	res.status(404).send('Unable to find the requested resource!');
+	res.status(404).sendFile('/app/src/routes/404.json');
 });
 
 db.init().then(() => {
